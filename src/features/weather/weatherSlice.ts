7d5b40be4dbca5955261ec13ce4cfbfdd7eb0153
kashirskii <kashirskiy.yegor@ya.ru>
2024-11-07 02:46:33 +0400

    add: error handling

--- src/features/weather/weatherSlice.ts
+++ src/features/weather/weatherSlice.ts
@@ -1,22 +1,35 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { getCurrentWeatherRequest, getCurrentWeatherResponse } from "../../../types";
-import { getCurrentWeather } from "../../api/getCurrentWeather";
+import {
+  getCurrentWeatherRequest,
+  getCurrentWeatherResponse,
+} from "../../../types";
 
-export const fecthDataWeather = createAsyncThunk(
-  "weather/fetchDataWeather",
-  async ({q, days, lang}: getCurrentWeatherRequest, _) => {
-    return getCurrentWeather({q, days, lang}).then(response => response.data);
-  }
-);
+import { getCurrentWeather } from "../../api/getCurrentWeather";
+import { AxiosError } from "axios";
+
+export const fecthDataWeather = createAsyncThunk<
+  getCurrentWeatherResponse,
+  getCurrentWeatherRequest,
+  { rejectValue: { status: AxiosError["status"] } }
+>("weather/fetchDataWeather", async ({ q, days, lang }, thunkAPI) => {
+  return getCurrentWeather({ q, days, lang })
+    .then((res) => res.data)
+    .catch((err) => {
+      const error = err as AxiosError;
+      return thunkAPI.rejectWithValue({ status: error.status });
+    });
+});
 
 export interface WeatherState {
-  data: getCurrentWeatherResponse | null;
+  data: getCurrentWeatherResponse | null | undefined;
   loading: boolean;
+  error: null | string | undefined;
 }
 
 const initialState: WeatherState = {
   data: null,
   loading: false,
+  error: null,
 };
 
 export const weatherSlice = createSlice({
@@ -30,10 +43,19 @@ export const weatherSlice = createSlice({
     builder.addCase(fecthDataWeather.fulfilled, (state, action) => {
       state.loading = false;
       state.data = action.payload;
-    })
+    });
+    builder.addCase(fecthDataWeather.rejected, (state, action) => {
+      state.loading = false;
+      state.data = null;
+      if (action.payload?.status === 400) {
+        state.error = "Геопозиция не найдена";
+      } else {
+        state.error = "Непредвиденная ошибка";
+      }
+    });
   },
 });
 
-export const {  } = weatherSlice.actions;
+export const {} = weatherSlice.actions;
 
 export default weatherSlice.reducer;
